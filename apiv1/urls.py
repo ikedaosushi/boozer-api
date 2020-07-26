cb05d4d7ebe9e97ad4c40ec1967a54ebf0994bd2
ikedaosushi <ikeda.yutaro@gmail.com>
2020-07-26 17:57:06 +0900

    Tmp

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -3,5 +3,5 @@ from .views import stations, election
 
 urlpatterns = [
     path('stations', stations),
-    path('election', election.run)
+    path('election', election)
 ]
--- apiv1/views.py
+++ apiv1/views.py
@@ -31,9 +31,28 @@ def stations(request):
     )
     return JsonResponse(resp, safe=False, json_dumps_params={'ensure_ascii': False})
 
+def _response400(self, message):
+    return JsonResponse(dict(message=message), safe=False, json_dumps_params={'ensure_ascii': False}, status=400)
 
-class Election:
-    def __init__(self):
+
+def election(request):
+    station_ids = request.GET.getlist('station_id')
+    if len(station_ids) < 2:
+        _response400("station_id is required at least 2")
+    try:
+        station_ids = [int(station_id) for station_id in station_ids]
+    except ValueError:
+        _response400(f"station_id is invalid: {station_ids}")
+    df_stations = read_frame(Station.objects.all())
+    results = Election(df_stations, station_ids).run()
+    response = dict(
+        station_ids=station_ids,
+        results=results
+    )
+    return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
+
+class EkispertRouteTime:
+    def __init__(self) -> None:
         self.session = HTMLSession()
         self.url = "https://roote.ekispert.net/ja/result"
         self.base_params = dict(
@@ -41,50 +60,52 @@ class Election:
             minute1=0, sort="time"
         )
 
-    def _partial_get_time(self, arr_station_id, dep_station_id):
-        key = "_".join(str(s_id) for s_id in sorted([arr_station_id, dep_station_id]))
-        if key in cache:
-            return arr_station_id, cache.get(key)
+    def get(self, arr_station_id, dep_station_id):
         params = dict(**self.base_params, arr_code=arr_station_id, dep_code=dep_station_id)
+        print(self.url)
         res = self.session.get(self.url, params=params)
         target_text = res.html.find('.candidate_list_txt > .orange_txt', first=True).text
         time = int(re.search(r'（\D*(\d+)分）', target_text).groups()[0])
 
+        return time
+
+
+class Election:
+    def __init__(self, df_stations, station_ids):
+        self.df_stations = df_stations
+        self.station_ids = station_ids
+        self.ekispert_route_time = EkispertRouteTime()
+
+    def _partial_get_time(self, arr_station_id, dep_station_id):
+        key = "_".join(str(s_id) for s_id in sorted([arr_station_id, dep_station_id]))
+        # if key in cache:
+        #     return arr_station_id, cache.get(key)
+        time = self.ekispert_route_time.get(arr_station_id, dep_station_id)
         cache.set(key, time, None)
         return arr_station_id, time
 
-    def _response400(self, message):
-        return JsonResponse(dict(message=message), safe=False, json_dumps_params={'ensure_ascii': False}, status=400)
-
-    def run(self, request):
-        station_ids = request.GET.getlist('station_id')
-        if len(station_ids) < 2:
-            self._response400("station_id is required at least 2")
-        try:
-            station_ids = [int(station_id) for station_id in station_ids]
-        except ValueError:
-            self._response400(f"station_id is invalid: {station_ids}")
-
-
-        df_stations = read_frame(Station.objects.all())
-        candidate_stations = df_stations.query("station_id in @station_ids")
-        print(station_ids)
-        print(candidate_stations)
+    def _station_with_distance(self):
+        candidate_stations = self.df_stations.query("station_id in @self.station_ids")
         halfway_coords = candidate_stations['lat'].mean(), candidate_stations['lon'].mean()
         ds = []
-        for coords in df_stations[['lat', 'lon']].values:
+        for coords in self.df_stations[['lat', 'lon']].values:
             d = distance.distance(halfway_coords, coords.tolist())
             ds.append(d)
 
-        stations = df_stations[['station_id', 'station_name', 'lat', 'lon', 'num_shop', 'top10_avarage_score']]
+        df_stations = self.df_stations[['station_id', 'station_name', 'lat', 'lon', 'num_shop', 'top10_avarage_score']]
+        df_stations['distance'] = ds
+
+        return df_stations
+
+    def run(self):
+        df_stations = self._station_with_distance()
         station_id_names = df_stations.set_index("station_id")['station_name'].to_dict()
-        stations['distance'] = ds
         results = []
-        for _, row in stations.sort_values('distance').head(10).iterrows():
+        for _, row in df_stations.sort_values('distance').head(10).iterrows():
             candidate_station_id = row['station_id']
             result = dict(candidate_station=row.drop("distance").to_dict())
-            for station_id in station_ids:
-                staion_times = [self._partial_get_time(station_id, candidate_station_id) for station_id in station_ids]
+            for station_id in self.station_ids:
+                staion_times = [self._partial_get_time(station_id, candidate_station_id) for station_id in self.station_ids]
                 result['station_times'] = [dict(station_id=t[0], station_name=station_id_names[t[0]], time=t[1]) for t in staion_times]
                 times = [r['time'] for r in result['station_times']]
                 sum_ = sum(times)
@@ -93,10 +114,5 @@ class Election:
             results.append(result)
             
         results = sorted(results, key=lambda result: result['indicater'])
-        res = dict(
-            station_ids=station_ids,
-            results=results
-        )
-        return JsonResponse(res, safe=False, json_dumps_params={'ensure_ascii': False})
 
-election = Election()
+        return results
